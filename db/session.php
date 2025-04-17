3315a41826d018198db406ead032baaa0a7c1af4
D <kasherkate26@gmail.com>
2025-04-17 15:30:25 +0200

    Add files via upload

---
+++ db/session.php
@@ -0,0 +1,50 @@
+<?php
+    if(session_status() !== PHP_SESSION_ACTIVE)
+    {
+        session_start();
+    }
+
+
+    function setCurrentUser($id)
+    {
+        $conn = new PDO("mysql:host=localhost:3306;dbname=getticket", "root", "root");
+
+        $_SESSION["id"] = $id;
+    }
+
+    function getCurrentUser()
+    {
+        $conn = new PDO("mysql:host=localhost:3306;dbname=getticket", "root", "root");
+
+        $stmt = $conn->prepare("SELECT * FROM users u INNER JOIN roles r ON u.id=r.id WHERE u.id=?");
+        $stmt->execute( [$_SESSION["id"]] );
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC)[0];
+    }
+
+    function getCurrentRank()
+    {
+        $conn = new PDO("mysql:host=localhost:3306;dbname=getticket", "root", "root");
+
+        $stmt = $conn->prepare("SELECT u.role_id FROM users u WHERE u.id=?");
+        $stmt->execute( [$_SESSION["id"]] );
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC)[0]["role_id"];
+    }
+
+    function getCurrentRankName()
+    {
+        $conn = new PDO("mysql:host=localhost:3306;dbname=getticket", "root", "root");
+
+        $stmt = $conn->prepare("SELECT r.name FROM users u INNER JOIN roles r ON u.id=r.id WHERE u.id=?");
+        $stmt->execute( [$_SESSION["id"]] );
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC)[0]["name"];
+    }
+
+    function logout()
+    {
+        session_unset();
+        session_destroy();
+    }
+?>
